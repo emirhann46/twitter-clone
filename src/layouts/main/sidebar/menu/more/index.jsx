37b6397254c0e5ad298be53ebe6d4fa14ac452a2
emirhan <fazliakkok@gmail.com>
2025-04-04 18:24:03 +0300

    Refactor CSS and Tailwind configuration for improved animations and utility classes; update More component for better interaction

--- src/layouts/main/sidebar/menu/more/index.jsx
+++ src/layouts/main/sidebar/menu/more/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
-
+import "~/index.css"
 function More() {
   return (
-    <Popover className="relative"> 
-      <PopoverButton className="py-1 block group">
-        <div className="p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
-          <div className="w-[26.25px] h-[26.25px] relative">
+    <Popover className="relative cursor-pointer">
+      <PopoverButton className="py-1 group cursor-pointer flex w-full">
+        <div className="p-3 rounded-full  cursor-pointer transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
+          <div className="w-[26.25px] h-[26.25px] relative  ">
             <svg viewBox="0 0 24 24" ><path fill="#fff" d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"></path></svg>
 
           </div>
-          <div className="pr-4 mb-2 text-xl">
+          <div className="pr-4 mb-2 text-xl ">
             Daha fazla
           </div>
 
@@ -18,15 +18,15 @@ function More() {
         </div>
 
       </PopoverButton>
-      <PopoverPanel className={"w-[318px] absolute bottom-0 left-0 bg-black shadow-lg rounded-lg p-4 mt-2  "}>
+      <PopoverPanel className={"w-[318px] absolute bottom-0 left-0 shadow-custom bg-black rounded-lg p-4 mt-2  "}>
         <div className="flex flex-col gap-2">
           <div className="text-sm text-gray-500 font-bold">Hesap</div>
           <div className="text-sm text-gray-500 font-bold">Listeler</div>
           <div className="text-sm text-gray-500 font-bold">Kayıtlı Tweetler</div>
           <div className="text-sm text-gray-500 font-bold">İlgili hesaplar</div>
           <div className="text-sm text-gray-500 font-bold">Karanlık mod</div>
-          </div>
-        </PopoverPanel>
+        </div>
+      </PopoverPanel>
     </Popover>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {
-      animation: {
-        'fade-in': 'fadeIn 0.8s ease-out',
-        'fly-1': 'fly 15s linear infinite',
-        'fly-2': 'fly 18s linear infinite',
-        'fly-3': 'fly 21s linear infinite',
-        'fly-4': 'fly 24s linear infinite',
-        'fly-5': 'fly 27s linear infinite',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0', transform: 'translateY(20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-        fly: {
-          '0%': {
-            left: '-50px',
-            transform: 'translateY(0) scale(0.8)'
-          },
-          '10%': {
-            transform: 'translateY(-20px) scale(1)'
-          },
-          '20%': {
-            transform: 'translateY(10px) scale(0.9)'
-          },
-          '30%': {
-            transform: 'translateY(-10px) scale(1.1)'
-          },
-          '40%': {
-            transform: 'translateY(15px) scale(1)'
-          },
-          '50%': {
-            transform: 'translateY(-15px) scale(0.9)'
-          },
-          '75%': {
-            transform: 'translateY(10px) scale(1)'
-          },
-          '100%': {
-            left: 'calc(100% + 50px)',
-            transform: 'translateY(0) scale(0.8)'
-          },
-        },
+      colors: {
+        primary: "#1DA1F2",
+        dark: "#1a91da",
+        light: "#a5d0f5",
+        lighter: "#D4E5F4",
       },
       boxShadow: {
-        'custom': 'rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px',
-      
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
-      
     },
   },
-  plugins: [],
-}
+  plugins: [],  
+};
